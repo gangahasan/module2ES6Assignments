84fff562e11a07d46359e15055a1d3dca4bc305b
gangahasan <rgangabhavani2@gmail.com>
2024-12-05 07:21:34 +0530

    changed to arrow fun

--- questionNo1.js
+++ questionNo1.js
@@ -1,12 +1,8 @@
 
 
-
-
-let ans1 = multiply(3,4);
-let ans2 = multiply();
-
-console.log(ans1,ans2);
-
-function multiply(a=1,b=1){
+let multiply = (a=1,b=1) =>{
     return a*b;
 }
+ let ans1 = multiply(3,4);
+ let ans2 = multiply();
+console.log(ans1,ans2);
